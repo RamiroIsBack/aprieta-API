872bc7fbfcd198f91490fc6f90ca67bb217b0821
Ramiro <ramiro.santamaria.alvarez@gmail.com>
2018-09-23 12:01:32 -0400

    setting the password for mongolab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,10 +4,14 @@ var mongoose = require("mongoose");
 //ramiro@c3po:~/mongo/bin$ ./mongod --dbpath ~/mongo-data
 
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  "mongodb://<RamiroIsBack>:<Albamola2017>@ds111993.mlab.com:11993/photo-api" ||
-    "mongodb://localhost:27017/Aprieta",
-  { useNewUrlParser: true }
-);
+try {
+  mongoose.connect(
+    "mongodb://ramiro:password@ds111993.mlab.com:11993/photo-api" ||
+      "mongodb://localhost:27017/Aprieta",
+    { useNewUrlParser: true }
+  );
+} catch (e) {
+  console.log("something went wrong connecting to mongolab: ", e);
+}
 
 module.exports = { mongoose };
