1b0cc6703678e54206f3b2c0ff48813f083b8f40
Ramiro <ramiro.santamaria.alvarez@gmail.com>
2018-09-26 10:02:21 -0400

    Hash user password before saving through mongoose middleware

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -73,6 +74,21 @@ UserSchema.statics.findByToken = function(token) {
     "tokens.access": "auth"
   });
 };
+//mongoose have middleware to run before, after its functions
+UserSchema.pre("save", function(next) {
+  var user = this;
+  if (user.isModified("password")) {
+    //we don't want to rehash a password so we only do it when is being modified in this call
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model("User", UserSchema);
 module.exports = { User };
